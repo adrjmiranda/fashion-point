970fe391be8f4928fc29fbe7b95ab245df9dc136
Adriano Miranda <adrjmiranda@gmail.com>
2024-05-30 15:47:12 -0300

    chore: Improved product image preview script to switch between chosen images

--- tmp/js/img-preview.js
+++ tmp/js/img-preview.js
@@ -1,21 +1,60 @@
-const previewContainer = document.querySelector('#main-img-preview');
+const previewContainer = document.querySelector('#main-img');
 const imgInput = document.querySelector('#images');
+const imgThumbnails = document.querySelector('#thumbnails');
+const imtThumbItems = document.querySelectorAll('#thumbnails .item');
 
 imgInput &&
 	imgInput.addEventListener('change', (event) => {
-		const file = event.target.files[0];
+		const files = event.target.files;
 
-		if (file) {
-			if (!file.type.startsWith('image/')) {
-				alert('Please select a valid image file');
-				return;
-			}
+		const filesArray = [...files];
 
-			const reader = new FileReader();
-			reader.onload = function (e) {
-				previewContainer.style.backgroundImage = `url(${e.target.result})`;
-			};
-
-			reader.readAsDataURL(file);
+		if (filesArray.length > 4) {
+			alert('Please select a maximum of 4 images');
+			imgInput.value = null;
+			return;
 		}
+
+		if (filesArray.length > 0) {
+			filesArray.forEach((file, index) => {
+				if (file) {
+					if (!file.type.startsWith('image/')) {
+						alert('Please select a valid image file');
+						return;
+					}
+
+					const item = document.createElement('img');
+
+					const reader = new FileReader();
+					reader.onload = function (e) {
+						imtThumbItems[
+							index
+						].style.backgroundImage = `url(${e.target.result})`;
+
+						if (index == 0) {
+							previewContainer.style.backgroundImage = `url(${e.target.result})`;
+							imtThumbItems[index].classList.add('active');
+						}
+					};
+
+					reader.readAsDataURL(file);
+
+					imgThumbnails.appendChild(item);
+				}
+			});
+		}
+
+		// if (file) {
+		// 	if (!file.type.startsWith('image/')) {
+		// 		alert('Please select a valid image file');
+		// 		return;
+		// 	}
+
+		// 	const reader = new FileReader();
+		// 	reader.onload = function (e) {
+		// 		previewContainer.style.backgroundImage = `url(${e.target.result})`;
+		// 	};
+
+		// 	reader.readAsDataURL(file);
+		// }
 	});
